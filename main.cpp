6583146c10d5656140274327fc9002fffc0aea53
Josh Mann <65541818+jlmdev@users.noreply.github.com>
2022-08-26 10:44:04 -0400

    Example 1 without threading. Processes function1 and function2 sequentially.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+void function1() {
+    for (int i = 0; i < 200; i++) {
+        std::cout << "+";
+    }
+}
+
+void function2() {
+    for (int i = 0; i < 200; i++) {
+        std::cout << "-";
+    }
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    function1();
+    function2();
     return 0;
 }
